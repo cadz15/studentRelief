83a2fab9b7910f040d12b674d218477f27c1aa2a
Junatan Marc Cadayona <m4rc24@yahoo.com>
2025-06-16 15:54:21 +0800

    update logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'FCA') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=Dosis:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="icon" href="https://fcanationalservice.org/storage/uploads/site-logo.png">
         <!-- Scripts -->
         @routes
         @viteReactRefresh
